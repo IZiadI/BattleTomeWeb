ce0f1b87ffb12db83a4e2ba7a4d220bb6a176587
IZiadI <ziadashraf727@gmail.com>
2026-04-15 21:42:56 +0200

    Qol Improvements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3661417922"
+    serviceWorkerVersion: "3254278078"
   }
 });
